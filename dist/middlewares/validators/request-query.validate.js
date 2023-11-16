c52ebea370c059ac33e806a6266d08a15c2d1f73
ShashwotBhattarai <shashwot.tech@gmail.com>
2023-11-17 02:45:41 +0545

    validation complete

--- dist/middlewares/validators/request-query.validate.js
+++ dist/middlewares/validators/request-query.validate.js
@@ -1,95 +1,112 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.validateRequestQuery = void 0;
+const joi_1 = __importDefault(require("joi"));
+const conditions = [
+    "title",
+    "product_type",
+    "product_category",
+    "vendor",
+    "tags",
+    "price",
+    "compare-at-price",
+    "weight",
+    "inventory_stock",
+    "variants_title",
+];
+const operators = {
+    title: [
+        "is equal to",
+        "is not equal to",
+        "starts with",
+        "ends with",
+        "contains",
+        "does not contain",
+    ],
+    product_type: [
+        "is equal to",
+        "is not equal to",
+        "starts with",
+        "ends with",
+        "contains",
+        "does not contain",
+    ],
+    product_category: ["is equal to"],
+    vendor: [
+        "is equal to",
+        "is not equal to",
+        "starts with",
+        "ends with",
+        "contains",
+        "does not contain",
+    ],
+    tags: ["is equal to"],
+    price: ["is equal to", "is not equal to", "is greater than", "is less than"],
+    compare_at_price: [
+        "is equal to",
+        "is not equal to",
+        "is greater than",
+        "is less than",
+        "is not empty",
+        "is empty",
+    ],
+    weight: ["is equal to", "is not equal to", "is greater than", "is less than"],
+    inventory_stock: ["is equal to", "is greater than", "is less than"],
+    variants_title: [
+        "is equal to",
+        "is not equal to",
+        "starts with",
+        "ends with",
+        "contains",
+        "does not contain",
+    ],
+};
+const conditionSchema = joi_1.default.string().valid(...conditions);
+// Define the Joi schema for operators
+const operatorSchema = joi_1.default.object().keys({
+    title: joi_1.default.string().valid(...operators.title),
+    product_type: joi_1.default.string().valid(...operators.product_type),
+    product_category: joi_1.default.string().valid(...operators.product_category),
+    vendor: joi_1.default.string().valid(...operators.vendor),
+    tags: joi_1.default.string().valid(...operators.tags),
+    price: joi_1.default.string().valid(...operators.price),
+    compare_at_price: joi_1.default.string().valid(...operators.compare_at_price),
+    weight: joi_1.default.string().valid(...operators.weight),
+    inventory_stock: joi_1.default.string().valid(...operators.inventory_stock),
+    variants_title: joi_1.default.string().valid(...operators.variants_title),
+});
 const validateRequestQuery = (req, res, next) => {
-    const conditions = [
-        "title",
-        "product_type",
-        "product_category",
-        "vendor",
-        "tags",
-        "price",
-        "compare-at-price",
-        "weight",
-        "inventory_stock",
-        "variants_title",
-    ];
-    const operators = {
-        title: [
-            "is equal to",
-            "is not equal to",
-            "starts with",
-            "ends with",
-            "contains",
-            "does not contain",
-        ],
-        product_type: [
-            "is equal to",
-            "is not equal to",
-            "starts with",
-            "ends with",
-            "contains",
-            "does not contain",
-        ],
-        product_category: ["is equal to"],
-        vendor: [
-            "is equal to",
-            "is not equal to",
-            "starts with",
-            "ends with",
-            "contains",
-            "does not contain",
-        ],
-        tags: ["is equal to"],
-        price: [
-            "is equal to",
-            "is not equal to",
-            "is greater than",
-            "is less than",
-        ],
-        compare_at_price: [
-            "is equal to",
-            "is not equal to",
-            "is greater than",
-            "is less than",
-            "is not empty",
-            "is empty",
-        ],
-        weight: [
-            "is equal to",
-            "is not equal to",
-            "is greater than",
-            "is less than",
-        ],
-        inventory_stock: ["is equal to", "is greater than", "is less than"],
-        variants_title: [
-            "is equal to",
-            "is not equal to",
-            "starts with",
-            "ends with",
-            "contains",
-            "does not contain",
-        ],
-    };
-    /*
-    conditions array must be refactored to match the keys in database, since the same "condition"s are used to make the query
-    after refactoring the conditions array we must refactor the operations object too,
-    we must carefully refactor the keys of operatior object to match the conditons in conditions array,
-    additionally, logic for validating value must be revisited for automatic selection between number and string,
-    for now it accepts both string and number
-  */
     const { logic, queries } = req.body;
+    // Define the schema for the entire request body
+    const schema = joi_1.default.object({
+        logic: joi_1.default.string().required(),
+        queries: joi_1.default.array().items(joi_1.default.object({
+            condition: conditionSchema.required(),
+            operator: joi_1.default.string().required(),
+            value: joi_1.default.string().required(), // You may want to adjust this based on your actual operators
+        })),
+    });
+    // Validate the entire request body against the schema
+    const { error } = schema.validate(req.body);
+    if (error) {
+        return res
+            .status(400)
+            .json({ error: error.details.map((detail) => detail.message) });
+    }
+    // Additional validation for conditions and operators
     for (const element of queries) {
-        const req_condition = element.condition;
-        const req_operator = element.operator;
-        console.log(req_condition);
-        console.log(req_operator);
-        const req_condition_exists = conditions.includes(req_condition);
-        const req_operator_exists = operators[req_condition].includes(req_operator);
-        console.log(req_condition_exists);
-        console.log(req_operator_exists);
-        if (!(req_condition_exists && req_operator_exists)) {
-            return res.status(400).json({ message: { "Bad Request": element } });
+        const { error: conditionError } = conditionSchema.validate(element.condition);
+        const { error: operatorError } = operatorSchema.validate({
+            [element.condition]: element.operator,
+        });
+        if (conditionError || operatorError) {
+            return res.status(400).json({
+                conditionErrorerror: conditionError === null || conditionError === void 0 ? void 0 : conditionError.details.map((detail) => detail.message),
+                operatorError: operatorError === null || operatorError === void 0 ? void 0 : operatorError.details.map((detail) => detail.message),
+            });
         }
     }
     next();
--- src/middlewares/validators/request-query.validate.ts
+++ src/middlewares/validators/request-query.validate.ts
@@ -1,108 +1,124 @@
-// middleware.ts
 import { Request, Response, NextFunction } from "express";
-import * as Joi from "joi";
+import Joi from "joi";
 
+const conditions = [
+  "title",
+  "product_type",
+  "product_category",
+  "vendor",
+  "tags",
+  "price",
+  "compare-at-price",
+  "weight",
+  "inventory_stock",
+  "variants_title",
+];
+
+const operators: Record<string, string[]> = {
+  title: [
+    "is equal to",
+    "is not equal to",
+    "starts with",
+    "ends with",
+    "contains",
+    "does not contain",
+  ],
+  product_type: [
+    "is equal to",
+    "is not equal to",
+    "starts with",
+    "ends with",
+    "contains",
+    "does not contain",
+  ],
+  product_category: ["is equal to"],
+  vendor: [
+    "is equal to",
+    "is not equal to",
+    "starts with",
+    "ends with",
+    "contains",
+    "does not contain",
+  ],
+  tags: ["is equal to"],
+  price: ["is equal to", "is not equal to", "is greater than", "is less than"],
+  compare_at_price: [
+    "is equal to",
+    "is not equal to",
+    "is greater than",
+    "is less than",
+    "is not empty",
+    "is empty",
+  ],
+  weight: ["is equal to", "is not equal to", "is greater than", "is less than"],
+  inventory_stock: ["is equal to", "is greater than", "is less than"],
+  variants_title: [
+    "is equal to",
+    "is not equal to",
+    "starts with",
+    "ends with",
+    "contains",
+    "does not contain",
+  ],
+};
+
+const conditionSchema = Joi.string().valid(...conditions);
+
+const operatorSchema = Joi.object().keys({
+  title: Joi.string().valid(...operators.title),
+  product_type: Joi.string().valid(...operators.product_type),
+  product_category: Joi.string().valid(...operators.product_category),
+  vendor: Joi.string().valid(...operators.vendor),
+  tags: Joi.string().valid(...operators.tags),
+  price: Joi.string().valid(...operators.price),
+  compare_at_price: Joi.string().valid(...operators.compare_at_price),
+  weight: Joi.string().valid(...operators.weight),
+  inventory_stock: Joi.string().valid(...operators.inventory_stock),
+  variants_title: Joi.string().valid(...operators.variants_title),
+});
 export const validateRequestQuery = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const conditions = [
-    "title",
-    "product_type",
-    "product_category",
-    "vendor",
-    "tags",
-    "price",
-    "compare-at-price",
-    "weight",
-    "inventory_stock",
-    "variants_title",
-  ];
-
-  const operators: Record<string, string[]> = {
-    title: [
-      "is equal to",
-      "is not equal to",
-      "starts with",
-      "ends with",
-      "contains",
-      "does not contain",
-    ],
-    product_type: [
-      "is equal to",
-      "is not equal to",
-      "starts with",
-      "ends with",
-      "contains",
-      "does not contain",
-    ],
-    product_category: ["is equal to"],
-    vendor: [
-      "is equal to",
-      "is not equal to",
-      "starts with",
-      "ends with",
-      "contains",
-      "does not contain",
-    ],
-    tags: ["is equal to"],
-    price: [
-      "is equal to",
-      "is not equal to",
-      "is greater than",
-      "is less than",
-    ],
-    compare_at_price: [
-      "is equal to",
-      "is not equal to",
-      "is greater than",
-      "is less than",
-      "is not empty",
-      "is empty",
-    ],
-    weight: [
-      "is equal to",
-      "is not equal to",
-      "is greater than",
-      "is less than",
-    ],
-    inventory_stock: ["is equal to", "is greater than", "is less than"],
-    variants_title: [
-      "is equal to",
-      "is not equal to",
-      "starts with",
-      "ends with",
-      "contains",
-      "does not contain",
-    ],
-  };
-  /* 
-  conditions array must be refactored to match the keys in database, since the same "condition"s are used to make the query
-  after refactoring the conditions array we must refactor the operations object too, 
-  we must carefully refactor the keys of operatior object to match the conditons in conditions array,
-  additionally, logic for validating value must be revisited for automatic selection between number and string,
-  for now it accepts both string and number
-*/
-
   const { logic, queries } = req.body;
 
+  const schema = Joi.object({
+    logic: Joi.string().required(),
+    queries: Joi.array().items(
+      Joi.object({
+        condition: conditionSchema.required(),
+        operator: Joi.string().required(),
+        value: Joi.string().required(),
+      })
+    ),
+  });
+
+  const { error } = schema.validate(req.body);
+
+  if (error) {
+    return res
+      .status(400)
+      .json({ error: error.details.map((detail) => detail.message) });
+  }
+
   for (const element of queries) {
-    const req_condition = element.condition;
-    const req_operator = element.operator;
+    const { error: conditionError } = conditionSchema.validate(
+      element.condition
+    );
+    const { error: operatorError } = operatorSchema.validate({
+      [element.condition]: element.operator,
+    });
 
-    console.log(req_condition);
-    console.log(req_operator);
-
-    const req_condition_exists= conditions.includes(req_condition);
-    const req_operator_exists= operators[req_condition].includes(req_operator);
-
-    console.log(req_condition_exists);
-    console.log(req_operator_exists);
-
-    if (!(req_condition_exists && req_operator_exists)) { 
-      return res.status(400).json({ message: {"Bad Request":element} });
+    if (conditionError || operatorError) {
+      return res.status(400).json({
+        conditionErrorerror: conditionError?.details.map(
+          (detail) => detail.message
+        ),
+        operatorError: operatorError?.details.map((detail) => detail.message),
+      });
     }
   }
-   next();
+
+  next();
 };
